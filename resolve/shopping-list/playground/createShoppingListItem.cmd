e7d2a3c095d10fa4244fbabc521f6b0ceeb9082a
Dan Ignatov <ignatovdan@gmail.com>
2022-02-21 13:50:41 +0300

    Typo

--- resolve/shopping-list/playground/createShoppingListItem.cmd
+++ resolve/shopping-list/playground/createShoppingListItem.cmd
@@ -1,1 +1,1 @@
-curl http://localhost:3000/api/commands  -H "Content-Type: application/json" -X POST  -d createShoppingListItem.xml
+curl http://localhost:3000/api/commands  -H "Content-Type: application/json" -X POST  -d @createShoppingListItem.xml
