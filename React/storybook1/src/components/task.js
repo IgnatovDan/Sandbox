5c1a0d14505dd1763617fdd1cbd5f24a8ab09187
Dan Ignatov <ignatovdan@gmail.com>
2022-03-24 11:33:30 +0400

    Typo for list item's 'star' class name

--- React/storybook1/src/components/task.js
+++ React/storybook1/src/components/task.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 export default function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
     return (
-        <div className={ `list-item {$state}` }>
+        <div className={ `list-item ${state}` }>
             <label className='checkbox'>
                 <input type="checkbox" defaultChecked={ state === 'TASK_ARCHIVED' }
                     disabled={ true } name='checked' />
