cf9a9b4faa28c68936fb57079657f9aad9bee661
Dan Ignatov <ignatovdan@gmail.com>
2022-03-24 11:58:17 +0400

    Comment 'import PropTypes'

--- React/storybook1/src/components/task.js
+++ React/storybook1/src/components/task.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+
 
 export default function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
     return (
