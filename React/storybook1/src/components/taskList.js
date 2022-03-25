a51270ada53ee3c849c19c52b9c185380bf93d93
Dan Ignatov <ignatovdan@gmail.com>
2022-03-25 14:10:13 +0400

    Remove Redux related code from TaskList

--- React/storybook1/src/components/taskList.js
+++ React/storybook1/src/components/taskList.js
@@ -1,8 +1,87 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { connect } from 'react-redux';
 import { updateTaskState, TaskStates } from '../lib/store';
 
 import Task from './task';
+
+export const TaskBoxStatuses = {
+    loading: 'loading',
+    ready: 'ready',
+};
+
+//
+// Pure UI component, no refs to Redux
+//
+export function TaskList({ tasks, taskBoxStatus, pinTask, archiveTask }) {
+    const loadingRow = (
+        <div className='loading-item'>
+            <span className='glow-checkbox' />
+            <span className='glow-text'>
+                <span>Loading</span> <span>cool</span> <span>state</span>
+            </span>
+        </div>
+    );
+
+    if (taskBoxStatus === 'loading') {
+        return (
+            <div className='list-items' data-testingid='loading' key={ 'loading' }>
+                { loadingRow }
+                { loadingRow }
+                { loadingRow }
+            </div>
+        );
+    } else if (tasks.length === 0) {
+        return (
+            <div className='list-items' key={ 'empty' } data-testingid='empty'>
+                <div className='wrapper-message'>
+                    <span className='icon-check' />
+                    <div className='title-message'>You have no tasks</div>
+                    <div className='subtitle-message'>Sit back and relax</div>
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <div className='listItems' data-testid='success' key={ 'success' }>
+                {
+                    tasks.map(task => {
+                        return (
+                            <Task
+                                key={ task.id }
+                                task={ task }
+                                onPinTask={ taskId => pinTask(taskId) }
+                                onArchiveTask={ taskId => archiveTask(taskId) }
+                            />
+                        );
+                    })
+                }
+            </div>
+        );
+    }
+};
+
+const mapStateToProps = (state) => {
+    return {
+        tasks: null /* TODO: useSelector(state => {
+            return [
+                ...state.tasks.filter(task => (task.state === TaskStates.inbox || task.state === TaskStates.pinned))
+            ]
+        })*/,
+        taskBoxStatus: null /* TODO useSelector(state => state.taskbox)*/,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+    pinTask: (taskId) => dispatch(updateTaskState({ id: taskId, newTaskState: TaskStates.pinned })),
+    archiveTask: (taskId) => dispatch(updateTaskState({ id: taskId, newTaskState: TaskStates.archived })),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TaskList);
+
+
+/*
+
+original version with embedded redux code
 
 export default function TaskList() {
     const tasks = useSelector(state => {
@@ -71,3 +150,5 @@ export default function TaskList() {
         </div>
     );
 };
+
+*/
