65d16f35c8e238833e98a019a48bf71da6644724
Dan Ignatov <ignatovdan@gmail.com>
2022-04-04 21:32:51 +0400

    Touch Slider & "icons-material"

--- React/mui1/src/App.js
+++ React/mui1/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Button, Autocomplete, TextField, ButtonGroup, Checkbox, Box, Stack, Slider } from '@mui/material';
-// import VolumeDown from '@mui/icons-material/VolumeDown';
-// import VolumeUp from '@mui/icons-material/VolumeUp';
+import VolumeDown from '@mui/icons-material/VolumeDown';
+import VolumeUp from '@mui/icons-material/VolumeUp';
 
 function App() {
-  // const [sliderValue, setSliderValue] = React.useState();
-  // const handleSliderChange = (e, newValue) => {
-  //   setSliderValue(newValue);
-  // };
+  const [sliderValue, setSliderValue] = React.useState();
+  const handleSliderChange = (e, newValue) => {
+    setSliderValue(newValue);
+  };
 
   return (
     <div className="App">
@@ -29,14 +29,13 @@ function App() {
 
       <Checkbox style={ { display: 'block' } } inputProps={ { 'aria-label': 'Checkbox demo' } } defaultChecked color="success" />
 
-      {/* <Box sx={ { width: 200 } }>
+      <Box sx={ { width: 200 } }>
         <Stack spacing={ 2 } direction="row" sx={ { mb: 1 } } alignItems="center">
           <VolumeDown />
           <Slider aria-label="Volume" value={ sliderValue } onChange={ handleSliderChange } />
           <VolumeUp />
         </Stack>
-        <Slider disabled defaultValue={ 30 } aria-label="Disabled slider" />
-      </Box> */}
+      </Box>
     </div>
   );
 }
