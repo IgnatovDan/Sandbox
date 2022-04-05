7fe96957a0d6b0549d86eb35fc8fa90e28a63514
Dan Ignatov <ignatovdan@gmail.com>
2022-04-06 00:42:20 +0400

    Typo

--- Stripe/Stripe1/showCustomers.js
+++ Stripe/Stripe1/showCustomers.js
@@ -13,14 +13,14 @@ console.log('Starting...');
         // Set key 'per request'
         apiKey: process.env.STRIPE_SECRET_KEY
     });
-    console.log(customers); // TODO: why there is no customers? "{ object: 'list', data: [], has_more: false, url: '/v1/customers' }"
+    console.log(customers); // TODO: why the list is empty? "{ object: 'list', data: [], has_more: false, url: '/v1/customers' }"
     console.log('< Get Customers');
 
     console.log('> Get Customer');
     const customer1 = await stripe.customers.retrieve('cus_LS1oQLpzydWGTu', {
         apiKey: process.env.STRIPE_SECRET_KEY
     });
-    console.log(customer1); // "name: 'Customer1'," and a lot of other info is printed
+    console.log(customer1); // "name: 'Customer1'," and a lot of other info are logged
     console.log('< Get Customer');
 })()
 
