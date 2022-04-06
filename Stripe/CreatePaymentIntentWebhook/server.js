f9ce5bda477ac372ca0f65045639b63206974d52
Dan Ignatov <ignatovdan@gmail.com>
2022-04-07 02:33:11 +0400

    Patch webhook

--- Stripe/CreatePaymentIntentWebhook/server.js
+++ Stripe/CreatePaymentIntentWebhook/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config({ path: './.env' });
-const app = require('express')();
+const express = require('express');
+const app = express();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
 const bodyParser = require("body-parser");
@@ -29,45 +30,40 @@ app.post('/create-payment-intent', async (req, res) => {
     res.json({ clientSecret: paymentIntent.client_secret });
 });
 
-app.post(
-    'webhook',
-    // Stripe requires the raw body to construct the event
-    bodyParser.raw({ type: 'application/json' }),
-    (req, res) => {
-        const sig = req.headers["stripe-signature"];
-        console.log(`> webhook`);
+app.post('/webhook', express.raw({ type: 'application/json' }), (req, res) => {
+    const sig = req.headers["stripe-signature"];
+    console.log(`> webhook`);
 
-        let event;
+    let event;
 
-        try {
-            event = stripe.webhooks.constructEvent(req.body, sig, webhookSecret);
-        } catch (err) {
-            // On error, log and return the error message
-            console.log(`❌ Error message: ${err.message}`);
-            return res.status(400).send(`Webhook Error: ${err.message}`);
-        }
-
-        // Successfully constructed event
-        console.log("✅ Success:", event.id);
-
-        // Handle the event
-        switch (event.type) {
-            case 'payment_intent.created':
-                console.log('payment_intent.created');
-                break;
-            case 'payment_intent.succeeded':
-                const paymentIntent = event.data.object;
-                console.log(paymentIntent);
-                // Then define and call a function to handle the event payment_intent.succeeded
-                break;
-            // ... handle other event types
-            default:
-                console.log(`Unhandled event type ${event.type}`);
-        }
-
-        // Return a response to acknowledge receipt of the event
-        res.json({ received: true });
+    try {
+        event = stripe.webhooks.constructEvent(req.body, sig, webhookSecret);
+    } catch (err) {
+        // On error, log and return the error message
+        console.log(`❌ Error message: ${err.message}`);
+        return res.status(400).send(`Webhook Error: ${err.message}`);
     }
-);
+
+    // Successfully constructed event
+    console.log("✅ Success:", event.id);
+    const paymentIntent = event.data.object;
+    console.log(paymentIntent);
+
+    // Handle the event
+    switch (event.type) {
+        case 'payment_intent.created':
+            console.log('payment_intent.created');
+            break;
+        case 'payment_intent.succeeded':
+            // Then define and call a function to handle the event payment_intent.succeeded
+            break;
+        // ... handle other event types
+        default:
+            console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a response to acknowledge receipt of the event
+    res.json({ received: true });
+});
 
 app.listen(1234, () => console.log('Listening on the 1234 port'));
