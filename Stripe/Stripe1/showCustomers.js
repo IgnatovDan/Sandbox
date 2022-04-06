4f624745ff0e640db3a60ca40a90bb05a1d631ae
Dan Ignatov <ignatovdan@gmail.com>
2022-04-06 12:34:07 +0400

    Split to showSomeThing files

--- Stripe/Stripe1/showCustomers.js
+++ Stripe/Stripe1/showCustomers.js
@@ -1,24 +1,13 @@
 require('dotenv').config();
 
-// Set key globally
-// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const stripe = require('stripe')();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 console.log('Starting...');
 (async () => {
     console.log('> Get Customers');
-    const customers = await stripe.customers.list({}, {
-        // Set key 'per request'
-        apiKey: process.env.STRIPE_SECRET_KEY
-    });
+
+    const customers = await stripe.customers.list();
     console.log(customers); // TODO: why the list is empty? "{ object: 'list', data: [], has_more: false, url: '/v1/customers' }"
     console.log('< Get Customers');
-
-    console.log('> Get Customer');
-    const customer1 = await stripe.customers.retrieve('cus_LS1oQLpzydWGTu', {
-        apiKey: process.env.STRIPE_SECRET_KEY
-    });
-    console.log(customer1); // "name: 'Customer1'," and a lot of other info are logged
-    console.log('< Get Customer');
 })();
 
