4c566736c4c4d1b5fb4469c33e98cceb95b4801e
Dan Ignatov <ignatovdan@gmail.com>
2022-04-12 11:10:31 +0300

    Add empty react app with the checkout button

---
+++ Stripe/CreateSubscriptionSession/Client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        Edit src/App.js and save to reload.
+      </p>
+      <button>Checkout</button>
+    </div>
+  );
+}
+
+export default App;
