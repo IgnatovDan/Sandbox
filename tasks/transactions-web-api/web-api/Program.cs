c47a75e1d0e24a23f5914c474de7c0ee376da42f
Ignatov Dan <ignatovdan@gmail.com>
2022-08-21 02:29:28 +0300

    Add 'throw new BadHttpRequestException' comment

--- tasks/transactions-web-api/web-api/Program.cs
+++ tasks/transactions-web-api/web-api/Program.cs
@@ -17,14 +17,18 @@ app.MapPost("/", ([FromQuery(Name = "insert")] string insert) => {
   // http://127.0.0.1:5000?insert={"id":"cfaa0d3f-7fea-4423-9f69-ebff826e2f89","operationDate":"2019-04-02T13:10:20.0263632+03:00","amount":23.05 }
   //
   if (string.IsNullOrEmpty(insert)) {
-    return Results.BadRequest(); // cannot process the request due to something that is perceived to be a client error
+    // cannot process the request due to something that is perceived to be a client error
+    // TODO: change to throw new BadHttpRequestException
+    return Results.BadRequest();
   }
 
   try {
     var entity = JsonSerializer.Deserialize<Entity>(insert);
   }
   catch {
-    return Results.BadRequest(); // cannot process the request due to something that is perceived to be a client error
+    // cannot process the request due to something that is perceived to be a client error
+    // TODO: change to throw new BadHttpRequestException
+    return Results.BadRequest();
   }
 
   return Results.Ok();
