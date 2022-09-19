3f39abf1de1614637766e8ab3bdaa00011ecfeb8
Ignatov Dan <ignatovdan@gmail.com>
2022-09-19 22:11:02 +0300

    Fill console-utils-test

--- tasks/figure-area/console-commands-lib-tests/console-utils-test.cs
+++ tasks/figure-area/console-commands-lib-tests/console-utils-test.cs
@@ -6,9 +6,33 @@ namespace console_commands_lib_tests;
 
 public class ConsoleUtilsTest_ReadDoubleFromConsole {
   [Fact]
-  public void PassInteger() {
+  public void StringWithIntegerNumber() {
     var console = new Mock<IConsole>();
     console.Setup(o => o.ReadLine()).Returns("42");
+    double result = ConsoleUtils.ReadDoubleFromConsole(console.Object, "f1");
+    Assert.Equal(42, result);
+  }
+
+  [Fact]
+  public void StringWithFractionalNumber() {
+    var console = new Mock<IConsole>();
+    console.Setup(o => o.ReadLine()).Returns("42,2");
+    double result = ConsoleUtils.ReadDoubleFromConsole(console.Object, "f1");
+    Assert.Equal(42.2, result);
+  }
+
+  [Fact]
+  public void StringWithFractionalNumberIncorrectDelimiter() {
+    var console = new Mock<IConsole>();
+    console.SetupSequence(o => o.ReadLine()).Returns("42.2").Returns("0");
+    double result = ConsoleUtils.ReadDoubleFromConsole(console.Object, "f1");
+    Assert.Equal(0, result);
+  }
+
+  [Fact]
+  public void StringWithNonNumber() {
+    var console = new Mock<IConsole>();
+    console.SetupSequence(o => o.ReadLine()).Returns("qwe").Returns("42");
     double result = ConsoleUtils.ReadDoubleFromConsole(console.Object, "f1");
     Assert.Equal(42, result);
   }
