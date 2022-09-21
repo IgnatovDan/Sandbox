cc805c0e11f2e6b02dfd3adecf85d59a270f8ac4
Ignatov Dan <ignatovdan@gmail.com>
2022-09-22 01:56:47 +0300

    Typo

--- tasks/figure-area/figure-area-console-commands-lib/circle-area-command.cs
+++ tasks/figure-area/figure-area-console-commands-lib/circle-area-command.cs
@@ -5,7 +5,7 @@ namespace figure_area_console_commands_lib;
 
 public static class CircleAreaCommand {
   public static void Invoke(IConsoleService console) {
-    console.WriteLine("Для вычисления площади круга нужно ввести радиус круга'");
+    console.WriteLine("Для вычисления площади круга нужно ввести радиус круга");
     double radius = ConsoleUtils.ReadDoubleFromConsole(console, "радиус круга");
     double result = FigureArea.CalcCircleArea(radius);
     console.WriteLine("Площадь круга: " + result.ToString());
--- tasks/figure-area/figure-area-console-commands-lib/triangle-area-command.cs
+++ tasks/figure-area/figure-area-console-commands-lib/triangle-area-command.cs
@@ -5,7 +5,7 @@ namespace figure_area_console_commands_lib;
 
 public static class TriangleAreaCommand {
   public static void Invoke(IConsoleService console) {
-    console.WriteLine("Для вычисления площади треугольника нужно ввести длину основания и высоту'");
+    console.WriteLine("Для вычисления площади треугольника нужно ввести длину основания и высоту");
     double bottom = ConsoleUtils.ReadDoubleFromConsole(console, "длина основания треугольника");
     double height = ConsoleUtils.ReadDoubleFromConsole(console, "высота треугольника");
     double result = FigureArea.CalcTriangleAreaByBottomAndHeight(bottom, height);
