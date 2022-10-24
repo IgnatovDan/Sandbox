b89da50ebdf8862de526cbdb17f2d370c950aa55
Ignatov Dan <ignatovdan@gmail.com>
2022-10-24 16:38:07 +0300

    Update transaction_isolation_read_committed.sql

--- SQL/transaction_isolation_read_committed.sql
+++ SQL/transaction_isolation_read_committed.sql
@@ -24,7 +24,7 @@ insert into table1(name, value) values('to-update-2', '')
 insert into table1(name, value) values('to-delete-1', '')
 insert into table1(name, value) values('to-delete-2', '')
 select * from table1
--- id	name					value
+-- id	name
 -- 1	to-update-1	
 -- 2	to-update-2	
 -- 3	to-delete-1	
@@ -36,6 +36,7 @@ use [task-5BC3B61B-BCAA-4EBE-90FC-35C0BF657D88]
 
 set transaction isolation level read committed
 begin transaction T1
+select @@TRANCOUNT
 
 update table1 set value = value + '10' where name = 'to-update-1'
 insert into table1(name, value) values('inserted-1', '')
@@ -55,28 +56,31 @@ use [task-5BC3B61B-BCAA-4EBE-90FC-35C0BF657D88]
 
 set transaction isolation level read committed
 begin transaction T1 
+select @@TRANCOUNT
 
 select * from table1
--- lock, uncommitted changes in session 1
+-- lock, there are uncommitted changes (insert/update/delete) in session 1
 
 select * from table1 where name = 'to-update-2'
--- lock, uncommitted changes in session 1
+-- lock, there are uncommitted changes (insert/update/delete) in session 1
 
 update table1 set value = value + '20' where name = 'to-update-2'
--- lock, uncommitted changes in session 1
+-- lock, there are uncommitted changes (insert/update/delete) in session 1
 
 delete from table1 where name = 'to-delete-2'
--- lock, uncommitted changes in session 1
+-- lock, there are uncommitted changes (insert/update/delete) in session 1
 
-insert into table1(name, value) values('inserted20', '')
+insert into table1(name, value) values('inserted-2', '')
 -- succeess
 
 -- ============= Session 1 ============= 
-select * from table1
--- lock, uncommitted 'insert' in session 2
+> continue in session 1
 
-update table1 set value = value + '10' where name = 'to-update-1'
--- lock
+select * from table1
+-- lock, there is uncommitted 'insert' in session 2
+
+update table1 set value = value + ' 10' where name = 'to-update-1'
+-- dead lock, there is uncommitted 'insert' in session 2
 
 insert into table1(name, value) values('inserted-1-2', '')
 -- succeess
@@ -88,12 +92,15 @@ select * from table1
 -- lock, uncommitted 'insert' in session 2
 rollback transaction
 
--- ============= Session 1 ============= 
+-- ============= Session 2 ============= 
+> continue in session 2
+
 select * from table1
 -- 1	to-update-1	
 -- 2	to-update-2	
 -- 3	to-delete-1	
 -- 4	to-delete-2	
--- 6	inserted-2
+-- 6	inserted-2	
+-- visible uncommitted insert from session2
 
 rollback transaction
