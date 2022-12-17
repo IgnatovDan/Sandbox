b90808b08aa0d98e97166da907d88032f2fe8031
Ignatov Dan <ignatovdan@gmail.com>
2022-12-17 20:44:25 +0300

    Validate fonts

--- BEM/validate-bem/stylelint-plugins/validate-import-css-path.js
+++ BEM/validate-bem/stylelint-plugins/validate-import-css-path.js
@@ -5,14 +5,40 @@ const { report, ruleMessages, validateOptions } = stylelint.utils;
 const ruleName = 'validate-import-css-path/match-bem-rules';
 const messages = ruleMessages(ruleName, {
     invalidNormalizePath: (path) => `Expected "${path}" to be "../vendor/normalize.css"`,
-    normalizeBeforeBlocksFiles: (path) => `Expected "${path}" to be the included before 'blocks' files`,
-    invalidNestedBemPath: (path) => `Expected "${path}" to fit BEM nested folder structure rules`,
+    normalizeBeforeBlocksFiles: (path) => `Expected "${path}" to be included before 'blocks' files`,
+    fontsBeforeBlocksFiles: (path) => `Expected "${path}" to be included before 'blocks' files`,
+    fontsOutOfBlocks: (path) => `Expected fonts to be located out of the 'blocks' folder: "${path}"`,
+    //invalidNestedBemPath: (path) => `Expected "${path}" to fit BEM nested folder structure rules`,
 });
 
 function parseUriFromImportRule(importRule) {
     const paramsNodes = valueParser(importRule.params).nodes;
     const uri = paramsNodes[0].nodes[0].value;
     return uri;
+}
+
+function tryValidateNormalize({ importUri, result, rule, blocksStarted }) {
+    if (importUri.match("normalize.css")) {
+        if (blocksStarted) {
+            report({ ruleName, result, message: messages.normalizeBeforeBlocksFiles(importUri), node: rule, word: importUri });
+        }
+        if (importUri !== "../vendor/normalize.css") {
+            report({ ruleName, result, message: messages.invalidNormalizePath(importUri), node: rule, word: importUri });
+        }
+        return true;
+    }
+}
+
+function tryValidateFont({ importUri, result, rule, blocksStarted }) {
+    if (importUri.match("font") || importUri.match("inter")) {
+        if (blocksStarted) {
+            report({ ruleName, result, message: messages.fontsBeforeBlocksFiles(importUri), node: rule, word: importUri });
+        }
+        if (importUri.match("blocks")) {
+            report({ ruleName, result, message: messages.fontsOutOfBlocks(importUri), node: rule, word: importUri });
+        }
+        return true;
+    }
 }
 
 const ruleFunction = () => {
@@ -24,31 +50,14 @@ const ruleFunction = () => {
             if (String(importUri).match("blocks")) {
                 blocksStarted = true;
             }
-            if (String(importUri).match("normalize.css")) {
-                if (blocksStarted) {
-                    report({
-                        ruleName,
-                        result,
-                        message: messages.normalizeBeforeBlocksFiles(importUri),
-                        node: rule,
-                        word: importUri
-                    });
-                }
-                if (importUri !== "../vendor/normalize.css") {
-                    report({
-                        ruleName,
-                        result,
-                        message: messages.invalidNormalizePath(importUri),
-                        node: rule,
-                        word: importUri
-                    });
-                }
+            if (tryValidateNormalize({ importUri, result, rule, blocksStarted })) {
+                return;
             }
-            else if (String(importUri).match("font")) {
-                
+            if (tryValidateFont({ importUri, result, rule, blocksStarted })) {
+                return;
             }
-            else if (!String(importUri).match("../blocks/")) {
-                
+            if (!String(importUri).match("../blocks/")) {
+
             }
             else {
                 //console.log(importUri);
