717232c917e81966a6aedab7874a4210ed56b195
Ignatov Dan <ignatovdan@gmail.com>
2023-02-01 23:34:38 +0300

    Create index.js

---
+++ leetcode/linked-list/reverse/index.js
@@ -0,0 +1,24 @@
+/**
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  let newHead = null;
+  let currentHead = head;
+  while(currentHead) {
+    const prevHead = newHead;
+    newHead = new ListNode(currentHead.val, prevHead);
+    currentHead = currentHead.next;
+  }
+  return newHead;
+};
