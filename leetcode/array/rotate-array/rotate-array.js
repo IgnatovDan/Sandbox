d56b902242c85b1f69886cdf7998d3b7fb898a16
Ignatov Dan <ignatovdan@gmail.com>
2023-02-08 19:44:00 +0300

    Update rotate-array.js

--- leetcode/array/rotate-array/rotate-array.js
+++ leetcode/array/rotate-array/rotate-array.js
@@ -9,12 +9,55 @@ Given an integer array nums, rotate the array to the right by k steps, where k i
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-  //rotateByCicledStepByStepShift(nums, k);
-  rotateByIntermediateCopyShiftedItems(nums, k);
+  //rotateByOneStepShiftWithIntermediateValue(nums, k);
+  rotateByOneStepShiftWithFullCopy(nums, k);
+  //rotateByStepByStepShift(nums, k);
+  //rotateByIntermediateCopyShiftedItems(nums, k);
   //rotateByIntermediateCopyRotatedItems(nums, k);
 };
 
-function rotateByCicledStepByStepShift(nums, k) {
+function rotateByOneStepShiftWithIntermediateValue(nums, k) {
+  function getNewIndex(index, shiftPosition, arrayLength) {
+    return (index + shiftPosition) % arrayLength;
+  }
+  
+  if(!nums.length || nums.length == 1) {
+    return;
+  }
+  let shiftPosition = k % nums.length;
+  let numsCopy = [...nums];
+  
+  let newIndex = 0;
+  let currentValue = nums[0];
+  let i = 0;
+  do {
+    let currentIndex = newIndex;
+    newIndex = getNewIndex(currentIndex, shiftPosition, nums.length);
+    let intermediateValue = nums[newIndex];
+    nums[newIndex] = currentValue;
+    currentValue = intermediateValue;
+    // This will not work for ([1, 2, 3, 4], 2)
+    // Output:   [3, 2, 1, 4]
+    // Expected: [3, 4, 1, 2]
+    // Code doesn't enumerate each value
+  } while(newIndex != 0);
+}
+
+function rotateByOneStepShiftWithFullCopy(nums, k) {
+  function getNewIndex(index, shiftPosition, arrayLength) {
+    return (index + shiftPosition) % arrayLength;
+  }
+  
+  let shiftPosition = k % nums.length;
+  let numsCopy = [...nums];
+  
+  for(let i = 0; i < nums.length; i++) {
+    let newIndex = getNewIndex(i, shiftPosition, nums.length);
+    nums[newIndex] = numsCopy[i];
+  }
+}
+
+function rotateByStepByStepShift(nums, k) {
   //  Time Limit Exceeded for 11939 items in array
   if(!nums.length || nums.length == 1) {
     return;
