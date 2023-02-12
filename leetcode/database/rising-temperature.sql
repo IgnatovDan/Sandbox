85cf14107f4172d06a2adb80eb8c7f3a499f7f99
Ignatov Dan <ignatovdan@gmail.com>
2023-02-12 14:11:22 +0300

    Create rising-temperature.sql

---
+++ leetcode/database/rising-temperature.sql
@@ -0,0 +1,16 @@
+/*
+https://leetcode.com/problems/rising-temperature/
+Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+*/
+
+select WeatherToday.id from Weather as WeatherToday
+inner join Weather as WeatherYesterday on DATE_ADD(WeatherToday.recordDate, INTERVAL -1 DAY) = WeatherYesterday.recordDate
+where WeatherYesterday.temperature < WeatherToday.temperature;
+
+/*select * from Weather as WeatherToday
+where exists(
+    select 1 
+    from Wheather as WeatherYesterday 
+    where DATE_ADD(WeatherToday.recordDate, INTERVAL -1 DAY) = WeatherYesterday.recordDate
+        and WeatherToday.temperature > WeatherYesterday.temperature
+)*/
