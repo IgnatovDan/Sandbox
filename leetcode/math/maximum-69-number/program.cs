50656810c7b191ff26152e76d68aba30374dcc69
Ignatov Dan <ignatovdan@gmail.com>
2023-04-12 19:50:12 +0300

    Update program.cs

--- leetcode/math/maximum-69-number/program.cs
+++ leetcode/math/maximum-69-number/program.cs
@@ -8,6 +8,11 @@ Return the maximum number you can get by changing at most one digit (6 becomes 9
 
 public class Solution {
     public int Maximum69Number (int num) {
+        //return UseConvertToString(num);
+        return UseDevideReminder(num);
+    }
+
+    public int UseConvertToString (int num) {
         int result = num;
         string numAsDecimalString = num.ToString();
         int indexOfSix = numAsDecimalString.IndexOf('6');
@@ -17,5 +22,24 @@ public class Solution {
             result = int.Parse(new String(numAsChars));
         }
         return result;
+    }
+
+    private int UseDevideReminder(int num) {
+        int firstIndexOf6 = -1;
+        var currentNum = num;
+        var index = 0;
+        do {
+            int reminder = currentNum % 10;
+            if(reminder == 6) {
+                firstIndexOf6 = index;
+            }
+            index ++;
+            currentNum = currentNum / 10;
+        } while(currentNum > 0);
+        if(firstIndexOf6 != -1) {
+            return num - 6 * (int)Math.Pow(10, firstIndexOf6) + 9 * (int)Math.Pow(10, firstIndexOf6);
+        } else {
+            return num;
+        }
     }
 }
