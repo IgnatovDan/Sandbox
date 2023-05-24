d8930baa56ee3f712687df63b8d075113017c76b
Ignatov Dan <ignatovdan@gmail.com>
2023-05-24 10:27:22 +0300

    Add anchor download as File

--- Blazor/ServerDxXlxs/wwwroot/js/SaveAsFile.js
+++ Blazor/ServerDxXlxs/wwwroot/js/SaveAsFile.js
@@ -1,10 +1,17 @@
 function SaveAsFile(fileName, byteBase64) {
   var link = document.createElement('a');
   link.download = fileName;
+
+  /*
+  const file = new File([data], filename, {{ type: contentType }});
+  const exportUrl = URL.createObjectURL(file);
+  a.href = exportUrl;
+  a.target = '_blank';
+  */
   link.href = "data: application / vnd.openxmlformats - officedocument.spreadsheetml.sheet;" +
     "base64, " + byteBase64;  
             
   document.body.appendChild(link);
   link.click();
-  link.remove();
+  link.remove(); // setTimeout, URL.revokeObjectURL(url);
 }
