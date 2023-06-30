b3fdc5d7aee9ae9bd91d2c3e908b851c6525f147
Ignatov Dan <ignatovdan@gmail.com>
2023-06-30 11:08:39 +0300

    Update restart.cmd

--- Docker/Utils/restart.cmd
+++ Docker/Utils/restart.cmd
@@ -1,4 +1,5 @@
 REM Manually force memory limit to 8GB (if you have 16GB): https://learn.microsoft.com/en-us/windows/wsl/wsl-config#configure-global-options-with-wslconfig
+REM c:\users\%USERNAME%\.wslconfig
 
 REM Restart Docker system (then manually start it)
 wsl --shutdown
