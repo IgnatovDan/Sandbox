f307cf8c9b46120813691341648f88904cd12699
Ignatov Dan <ignatovdan@gmail.com>
2023-12-19 23:04:03 +0300

    Update async.cs

--- CS/Shorts/async.cs
+++ CS/Shorts/async.cs
@@ -1,3 +1,4 @@
+// t.Result
 async Task<int> AsyncFunc()
 {
   int i = 0;
@@ -15,4 +16,26 @@ Console.WriteLine("out2: " + t.Result);
 //async: 0
 //out1
 //async: 1
+//out2: 1
+
+//------------------------------------------------------------
+// await
+
+async Task<int> AsyncFunc()
+{
+  int i = 0;
+  Console.WriteLine("async: " + i);
+  await Task.Delay(1000);
+  i += 1;
+  Console.WriteLine("async: " + i);
+  return i;
+}
+
+var t = await AsyncFunc();
+Console.WriteLine("out1");
+Console.WriteLine("out2: " + t);
+
+//async: 0
+//async: 1
+//out1
 //out2: 1
