97307afc3984344447ec801f2b0be2e9e8265f06
Ignatov Dan <ignatovdan@gmail.com>
2024-06-03 02:13:37 +0300

    Add Windows docker container v1

---
+++ ASPNETCore_/MinApi/docker_windows/start.cmd
@@ -0,0 +1,9 @@
+docker-compose up --force-recreate --build
+REM docker-compose up --force-recreate --build --exit-code-from grpc_server_sandbox_20240601
+REM docker-compose up --force-recreate --build --abort-on-container-exit --exit-code-from grpc_server_sandbox_20240601
+
+REM keep containers started
+pause
+docker compose down -v
+REM keep containers logs
+pause
