deee7d3a0710b527e603a7ab7e0a4b4774723a0d
Ignatov Dan <ignatovdan@gmail.com>
2024-06-03 10:51:29 +0300

    Update aspnetcore minapi in docker

--- ASPNETCore_/MinApi/docker_windows/start.cmd
+++ ASPNETCore_/MinApi/docker_windows/start.cmd
@@ -1,7 +1,9 @@
 docker-compose up --force-recreate --build
-
 REM keep containers started
 pause
+
 docker compose down -v
-REM keep containers logs
+REM keep containers shutdown logs
 pause
+
+docker system prune --volumes --force
